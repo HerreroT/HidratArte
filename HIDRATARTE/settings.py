0769ec864b8dc9482c1d9423336cf3af3ea85f95
HerreroT <132868135+HerreroT@users.noreply.github.com>
2025-10-27 22:09:52 -0300

    V 1.0.5

--- HIDRATARTE/settings.py
+++ HIDRATARTE/settings.py
@@ -144,7 +144,9 @@ STORAGES = {
 
 # Media files (User uploaded files)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = Path(os.environ.get("MEDIA_ROOT", BASE_DIR / "media"))
+MEDIA_ROOT.mkdir(parents=True, exist_ok=True)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
