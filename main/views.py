b98f14a0797b07886f51a73f2542a6561c834132
HerreroT <132868135+HerreroT@users.noreply.github.com>
2025-10-28 09:53:04 -0300

    V 1.0.16

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 
 from django.apps import apps
 from django.http import HttpResponse
@@ -134,7 +134,7 @@ class PaymentMethodViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [AdminOrReadOnly]
-    parser_classes = (MultiPartParser, FormParser)
+    parser_classes = (MultiPartParser, FormParser, JSONParser)
 
     def create(self, request, *args, **kwargs):
         logger.debug(
